6bd3a0407a0d51d00ffb5c1d4512cc6c5f6acd85
nichels02 <130480483+nichels02@users.noreply.github.com>
2023-09-22 11:02:24 -0500

    parte3

--- src/common/MainFooter.tsx
+++ src/common/MainFooter.tsx
@@ -1,6 +1,10 @@
 function MainFooter(){
     return (
-        <></>
+        <footer id="main-footer">
+            <div className="container">
+                2023-Tecsup, todos los derechos reservados
+            </div>
+        </footer>
     )
 }
 export default MainFooter
--- src/common/MainNav.tsx
+++ src/common/MainNav.tsx
@@ -1,6 +1,21 @@
+import {Link} from "react-router-dom";
+
 function MainNav(){
     return (
-        <></>
+        <nav className='navbar navbar-expand-lg bg-body-tertiary sticky-top bg-dark'>
+            <div className='conteiner'>
+                <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/game">Game </Link>
+                    </li>
+                </ul>
+
+            </div>
+        </nav>
     )
 }
 export default MainNav
