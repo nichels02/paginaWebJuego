4104b064d359ee055050c1f90369e56aaa97f24b
nichels02 <130480483+nichels02@users.noreply.github.com>
2023-09-24 14:32:24 -0500

    tarea_semana_3

--- src/common/MainNav.tsx
+++ src/common/MainNav.tsx
@@ -1,16 +1,17 @@
 import {Link} from "react-router-dom";
+import './MainNav.css';
 
 function MainNav(){
     return (
         <nav className='navbar navbar-expand-lg bg-body-tertiary sticky-top bg-dark'>
             <div className='conteiner'>
                 <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-                    <li className="nav-item">
+                    <li className="nav1-item">
                         <Link className="nav-link" to="/">Home</Link>
                     </li>
 
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/game">Game </Link>
+                    <li className="nav1-item">
+                        <Link className="nav-link" to="/game">Game</Link>
                     </li>
                 </ul>
 
