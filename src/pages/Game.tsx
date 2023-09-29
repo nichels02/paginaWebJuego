a1748fed71bf0ea6d14ec108e53303d5c86b10a2
nichels02 <130480483+nichels02@users.noreply.github.com>
2023-09-29 12:04:42 -0500

    boton_de_hackeo

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,18 +1,24 @@
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 function Game(){
-    const { unityProvider } = useUnityContext({
+    const { unityProvider,sendMessage } = useUnityContext({
         loaderUrl: "/game.loader.js",
         dataUrl: "/game.data.unityweb",
         frameworkUrl: "/game.framework.js.unityweb",
         codeUrl: "/game.wasm.unityweb",
     });
 
+    function handleClickSpawn()
+    {
+        sendMessage("LaConexion", "crear_mensaje")
+    }
+
     return (
         <div className="centered-container">
             <div className="centered-content">
                 <h1 className="centered-title">lluvia acida</h1>
                 <Unity unityProvider={unityProvider} className="centered-unity" />
+                <button onClick={handleClickSpawn}>Spawn Enemies</button>
             </div>
         </div>
     );
