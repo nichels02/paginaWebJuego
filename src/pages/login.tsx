b4938543b774b0490e78c201d4e6ffb7a833452c
nichels02 <130480483+nichels02@users.noreply.github.com>
2023-11-10 11:50:24 -0500

    modificado el login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,106 @@
+import { useNavigate } from "react-router-dom";
+
+interface LoginProps {
+    onLogin: (result: any) => void;
+    isAuthenticated: boolean;
+}
+
+function Login({ onLogin, isAuthenticated }: LoginProps) {
+    const navigate = useNavigate();
+
+    const iniciarSesion = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const dataForm = new FormData(event.currentTarget);
+
+        const rutaServicio =
+            "https://www.wildcat.games/services/autorpgservices/login.php";
+
+        const requestData = {
+            usuario: dataForm.get("usuario"),
+            clave: dataForm.get("clave"),
+        };
+
+        const response = await fetch(rutaServicio, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(requestData),
+        });
+
+        const result = await response.json();
+
+        if (result.success) {
+            alert("Bienvenido");
+            onLogin(result);
+            navigate("/escritorio", { replace: true });
+        } else {
+            alert(result.message);
+        }
+    };
+
+    return (
+        <section className="padded">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4"></div>
+                    <div className="col-md-4">
+                        <h2 className="text-center">Iniciar sesión</h2>
+                        <form onSubmit={(event) => iniciarSesion(event)}>
+                            <div className="mb-2">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    required
+                                    name="usuario"
+                                    placeholder="Usuario"
+                                />
+                            </div>
+                            <div className="mb-2">
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    required
+                                    id="txtClave"
+                                    name="clave"
+                                    placeholder="Contraseña"
+                                />
+                                <input
+                                    type="checkbox"
+                                    className="form-check-input"
+                                    onClick={(event) =>
+                                        document
+                                            .getElementById("txtClave")
+                                            .setAttribute(
+                                                "type",
+                                                event.target.checked ? "text" : "password"
+                                            )
+                                    }
+                                />
+                                <label className="form-check-label">
+                                    &nbsp;Mostrar contraseña
+                                </label>
+                            </div>
+                            <div className="mb-2">
+                                <button type="submit" className="btn btn-primary w-100">
+                                    Iniciar sesión &gt;
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="col-md-4"></div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Login;
+
+
+
+
+/*
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 function Login(){
@@ -20,3 +123,5 @@ function Login(){
     );
     }
 export default Login
+
+*/
